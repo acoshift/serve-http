444425e84a8521d1f2bd0b64627c4ccb12167463
Thanatat Tamtan <acoshift@gmail.com>
2016-07-07 15:31:27 +0700

    Config fallback from one image

--- main.go
+++ main.go
@@ -6,26 +6,51 @@ import (
 	"path"
 	"strings"
 
+	"github.com/urfave/cli"
 	"github.com/urfave/negroni"
 )
 
-const (
-	dir       = http.Dir("www")
-	indexFile = "index.html"
-)
+type config struct {
+	Fallback bool
+	Index    string
+	Dir      http.Dir
+}
 
 func main() {
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{Name: "fallback"},
+		cli.StringFlag{Name: "index", Value: "index.html"},
+		cli.StringFlag{Name: "dir", Value: "www"},
+	}
+	app.Action = func(c *cli.Context) error {
+		run(config{
+			Fallback: c.Bool("fallback"),
+			Index:    c.String("index"),
+			Dir:      http.Dir(c.String("dir")),
+		})
+		return nil
+	}
+
+	app.Run(os.Args)
+}
+
+func run(cfg config) {
 	n := negroni.New(negroni.NewRecovery())
-	n.Use(&static{})
+	n.Use(&static{cfg})
+	if cfg.Fallback {
+		n.Use(&fallback{cfg})
+	}
 	n.UseHandler(http.NotFoundHandler())
 
 	n.Run(":80")
 }
 
 type static struct {
+	cfg config
 }
 
-func tryFile(file string) (http.File, os.FileInfo, error) {
+func tryFile(file string, dir http.Dir) (http.File, os.FileInfo, error) {
 	f, err := dir.Open(file)
 	if err != nil {
 		return nil, nil, err
@@ -47,12 +72,12 @@ func (s *static) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.Ha
 	file := r.URL.Path
 	addIndex := false
 
-	f, fi, err := tryFile(file)
+	f, fi, err := tryFile(file, s.cfg.Dir)
 	if err != nil {
 		// try .html
 		addIndex = true
 		file += ".html"
-		f, fi, err = tryFile(file)
+		f, fi, err = tryFile(file, s.cfg.Dir)
 		if err != nil {
 			next(rw, r)
 			return
@@ -68,8 +93,8 @@ func (s *static) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.Ha
 			return
 		}
 
-		file = path.Join(file, indexFile)
-		f, err = dir.Open(file)
+		file = path.Join(file, s.cfg.Index)
+		f, err = s.cfg.Dir.Open(file)
 		if err != nil {
 			next(rw, r)
 			return
@@ -84,4 +109,24 @@ func (s *static) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.Ha
 	}
 
 	http.ServeContent(rw, r, file, fi.ModTime(), f)
+}
+
+type fallback struct {
+	cfg config
+}
+
+func (s *fallback) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if r.Method != "GET" && r.Method != "HEAD" {
+		next(rw, r)
+		return
+	}
+
+	f, fi, err := tryFile(s.cfg.Index, s.cfg.Dir)
+	if err != nil {
+		next(rw, r)
+		return
+	}
+	defer f.Close()
+
+	http.ServeContent(rw, r, s.cfg.Index, fi.ModTime(), f)
 }
